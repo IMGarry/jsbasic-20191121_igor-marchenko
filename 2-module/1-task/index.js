5e08d9a7a27af5295cc18ace07a9a9dc24ba6b7e
Igor Marchenko <imarchenko.lf@gmail.com>
2019-12-22 22:29:25 +0300

    2 module complete

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,20 @@
  * @returns {Object}
  */
 function clone(obj) {
+  let cloneObj = {};
+  for (key in obj) {
+    if (null == obj[key]) {
+      cloneObj[key] = null;
+      continue;
+    }
+    if (typeof (obj[key]) == 'object') {
+      cloneObj[key] = {};
+      for (let newKey in obj[key]) {
+        cloneObj[key][newKey] = clone(obj[key][newKey]);
+      }
+    } else {
+      cloneObj[key] = obj[key];
+    }
+  }
+  return cloneObj;
 }
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -3,5 +3,13 @@
  * @param {Object} obj
  * @returns {Boolean}
  */
+
 function isEmpty(obj) {
+  if (Object.keys(obj).length === 0) {return true;}
+
+  for (let prop in obj) {
+    if (obj[prop] === undefined) {return false;}
+    if (obj[prop] !== null) {return false;}
+
+  }
 }
